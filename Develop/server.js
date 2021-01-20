a34c10e526cd4b9aa9b96d629b115641ff236c32
Muneeb Ali <muneebbwp@gmail.com>
2021-01-20 15:55:19 -0600

    added connection url for heroku

--- Develop/server.js
+++ Develop/server.js
@@ -21,3 +21,15 @@ require("./Routes/htmlRoutes.js")(app);
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb+srv://$[username]:$[password]@$[hostlist]/$[database]?retryWrites=true';
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  client.close();
+});
