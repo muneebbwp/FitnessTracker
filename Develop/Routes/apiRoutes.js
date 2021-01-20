f1c5ebf3c934e77ef6324d28dd0c86804b1f5770
Muneeb Ali <muneebbwp@gmail.com>
2021-01-20 17:46:07 -0600

    deploying

--- Develop/Routes/apiRoutes.js
+++ Develop/Routes/apiRoutes.js
@@ -1,4 +1,4 @@
-var db = require("../models");
+var db = require("../Models");
 
 module.exports = function(app) {
 
--- Develop/server.js
+++ Develop/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/fitnessTracker',
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
