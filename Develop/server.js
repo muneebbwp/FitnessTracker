36c0bbce30c22fc261516eb251756ff960aef0bb
Muneeb Ali <muneebbwp@gmail.com>
2021-01-20 15:43:53 -0600

    added models

--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,23 @@
 const express = require("express");
-const mongojs = require("mongojs");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
+const PORT = process.env.PORT || 3000;
 
-
-const databaseUrl = "fitnessTracker";
-const collections = ["workouts"];
-const db = mongojs(databaseUrl, collections);
 const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+const db = require("./models");
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
