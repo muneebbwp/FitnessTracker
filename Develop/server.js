33f23b9a338425835402e675eec1af43a985bd2d
Muneeb Ali <muneebbwp@gmail.com>
2021-01-20 16:10:54 -0600

    nothing

--- Develop/server.js
+++ Develop/server.js
@@ -12,15 +12,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
+
 
 const db = require("./models");
 
