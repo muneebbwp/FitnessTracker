60f8999d4c767233a897f84cd975e2db44088e38
Muneeb Ali <muneebbwp@gmail.com>
2021-01-23 11:09:41 -0600

    nothing much

--- server.js
+++ server.js
@@ -13,16 +13,6 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-  
-);
 const db = require("./models");
 
 require("./routes/apiRoutes")(app);
